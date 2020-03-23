8cfa2593f34546cb26ddb9288958966d64966cf1
skylible <sourcewidi@gmail.com>
2020-03-24 02:27:34 +0700

    Added get single product

--- salvager/views.py
+++ salvager/views.py
@@ -1,4 +1,6 @@
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import status
 from .models import Product, ProductImage
 from .serializers import ProductSerializer, ProductImageSerializer
 
@@ -9,6 +11,27 @@ class ListProductView(generics.ListAPIView):
     """
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+class ProductDetailView(generics.RetrieveDestroyAPIView):
+    """
+    GET products/:id/
+    PUT products/:id/
+    DELETE products/:id/
+    """
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def get(self, request, *args, **kwargs):
+        try:
+            a_product = self.queryset.get(pk=kwargs['pk'])
+            return Response(ProductSerializer(a_product).data)
+        except:
+            return Response(
+                data={
+                    "message": "Product with id: {} does not exist".format(kwargs['pk'])
+                },
+                status=status.HTTP_404_NOT_FOUND
+            )
 
 class ListProductImageView(generics.ListAPIView):
     """
