4d0c31a36914c3914c5fcdd3317e67e0b547cc58
skylible <sourcewidi@gmail.com>
2020-04-01 20:24:28 +0700

    Added Diary and Faq to admin

--- salvager/admin.py
+++ salvager/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Product, Instagram, ProductImage, Review
+from .models import Product, Instagram, ProductImage, Review, Diary, Faq
 
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(Instagram)
 admin.site.register(ProductImage)
 admin.site.register(Review)
+admin.site.register(Diary)
+admin.site.register(Faq)
