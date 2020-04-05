7b6056b114b33492719657b096da46369fd7dcfe
skylible <sourcewidi@gmail.com>
2020-04-05 16:36:45 +0700

    Added API endpoint for instagram

--- salvager/urls.py
+++ salvager/urls.py
@@ -4,11 +4,13 @@ from .views import ListProductImageView
 from .views import ProductDetailView
 from .views import ListFaqView
 from .views import ListDiaryView
+from .views import ListInstagramView
 
 urlpatterns = [
     path('products/', ListProductView.as_view(), name='product-all'),
     path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
     path('product-images/', ListProductImageView.as_view(), name='product-images-all'),
     path('faqs/', ListFaqView.as_view(), name='faq-all'),
+    path('instagrams/', ListInstagramView.as_view(), name='instagram-all'),
     path('diaries/', ListDiaryView.as_view(), name='diary-all')
 ]
